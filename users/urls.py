c4c9cf7b36713a739e21ecab2a87cc9d1b55aac4
badgersky <szymonek.ptasznik@gmail.com>
2023-05-04 22:45:47 +0200

    add logout view, update navbar

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.RegistrationView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
@@ -44,3 +44,10 @@ class LoginView(View):
                     return redirect(reverse('home:home'))
 
         return render(request, 'users/login.html', {'form': form})
+
+
+class LogoutView(View):
+
+    def get(self, request):
+        logout(request)
+        return redirect(reverse('home:home'))
