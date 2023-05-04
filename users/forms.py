61164abdc4e41156b2a5634ee9b99ddc2e1a8485
badgersky <szymonek.ptasznik@gmail.com>
2023-05-04 21:55:03 +0200

    update registration form

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from . import models
 
 
 class RegistrationForm(forms.ModelForm):
@@ -22,3 +25,40 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = get_user_model()
         fields = ('username', 'email', 'first_name', 'last_name', 'password', 'confirm_password')
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get('password')
+        confirm_password = self.cleaned_data.get('confirm_password')
+
+        if password and confirm_password and password != confirm_password:
+            raise ValidationError(f'Passwords don`t match')
+
+        validate_password(confirm_password)
+
+        return confirm_password
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+
+        if models.CustomUser.objects.filter(username=username).exists():
+            raise ValidationError(f'Try using different username')
+
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if models.CustomUser.objects.filter(email=email).exists():
+            raise ValidationError(f'Try using different email')
+
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.clean_confirm_password())
+        user.is_active = True
+
+        if commit:
+            user.save()
+
+        return user
