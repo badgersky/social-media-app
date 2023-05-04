fcf93b3f8af881097cd2ae95238e3b16fe4715f4
badgersky <szymonek.ptasznik@gmail.com>
2023-05-05 00:08:26 +0200

    create tweet model, register model, migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Tweet(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    likes = models.IntegerField(default=0, validators=[MinValueValidator(0)])
