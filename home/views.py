83769e31e35705a2bd10a134088609d95fc94351
badgersky <szymonek.ptasznik@gmail.com>
2023-05-05 22:39:24 +0200

    all posts render on home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from posts.models import Tweet
 
 
 class HomeView(View):
 
     def get(self, request):
-        return render(request, 'home/home.html')
+        tweets = Tweet.objects.all().order_by('-date')
 
+        return render(request, 'home/home.html', {'tweets': tweets})
