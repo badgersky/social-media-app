188f1412a4f998a9d209f4b558ee23e0f096451b
badgersky <szymonek.ptasznik@gmail.com>
2023-05-05 22:49:46 +0200

    add pagination to home view

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views import View
 from posts.models import Tweet
@@ -7,5 +8,9 @@ class HomeView(View):
 
     def get(self, request):
         tweets = Tweet.objects.all().order_by('-date')
+        paginator = Paginator(tweets, 10)
 
-        return render(request, 'home/home.html', {'tweets': tweets})
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'home/home.html', {'page_obj': page_obj})
