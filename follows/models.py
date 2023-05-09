f0e23b851af2cd158fe4d62e6c006a89b15a083f
badgersky <szymonek.ptasznik@gmail.com>
2023-05-09 13:43:34 +0200

    create model to track follows, migrate changes

--- follows/models.py
+++ follows/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Follow(models.Model):
+    followed_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='followed')
+    following_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='following')
