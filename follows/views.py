59776e28a9d80fc356ea571855e2b2fda26f9dfc
badgersky <szymonek.ptasznik@gmail.com>
2023-05-09 16:42:35 +0200

    create follow view

--- follows/views.py
+++ follows/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
 
-# Create your views here.
+from follows.models import Follow
+from users.models import CustomUser
+
+
+class FollowUser(View):
+
+    def get(self, request, user_id):
+        if request.user.is_authenticated:
+            try:
+                followed_user = CustomUser.objects.get(pk=user_id)
+            except CustomUser.DoesNotExist:
+                next_ = request.get('next')
+
+                messages.add_message(request,
+                                     messages.WARNING,
+                                     f'User you are trying to follow does not exist.'
+                                     )
+                return redirect(reverse(next_))
+
+            Follow.objects.create(followed_user=followed_user, following_user=request.user)
+            return redirect(reverse('users:profile', kwargs={'user_id': user_id}))
